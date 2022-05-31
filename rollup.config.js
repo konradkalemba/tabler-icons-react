22e172db53d58729d9a671241b4c5a71bde51272
Konrad Kalemba <konrad_kal@hotmail.com>
2022-05-31 08:40:51 +0200

    fix(*): fix tree-shakeability

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,28 @@
-import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs',
-  },
-  external: ['react', 'prop-types'],
+  output: [
+    {
+      file: 'dist/cjs/index.js',
+      format: 'cjs',
+    },
+    {
+      dir: 'dist',
+      format: 'es',
+      preserveModules: true,
+    },
+  ],
+  external: ['react'],
   plugins: [
+    resolve(),
     babel({
       exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
     }),
+    commonjs(),
   ],
 };
